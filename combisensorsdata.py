33ce90ca62cae926467c05e073dcd4764806565a
satyendra123 <Singhsatyendra885@gmail.com>
2024-04-30 13:36:41 +0530

    Update combisensorsdata.py

--- combisensorsdata.py
+++ combisensorsdata.py
@@ -51,36 +51,35 @@ import time
 uart = UART(2, baudrate=9600, tx=3, rx=1)
 uart1 = UART(1, baudrate=9600, tx=16, rx=17)
 sensor_requests = ['FA0101F9', 'FA0201FA']
-total_sensors = len(sensor_requests)
-total_engaged = 0
-total_disengaged = 0
-total_vacancy = 0
+sensor_status = []
 zone_id = '01'
 
 def calculate_sensor_status(response):
-    global total_engaged, total_disengaged, total_vacancy
-    status_byte = response[2:3]  # Convert to string
+    status_byte = response[2:3]
     if status_byte == b'\x01':
-        total_engaged += 1
+        return 1  # Engaged
     elif status_byte == b'\x00':
-        total_disengaged += 1
-
-    return total_sensors, total_engaged, total_disengaged, total_vacancy
+        return 0  # Disengaged
+    else:
+        return -1  # Invalid status
 
 def process_sensor_requests():
-    global total_engaged, total_disengaged, total_vacancy
-    total_engaged = 0
-    total_disengaged = 0
+    global sensor_status
+    sensor_status = []
     for request in sensor_requests:
         if request.startswith('FA'):
             uart.write(bytes.fromhex(request))
             time.sleep(2)
             response = uart.read()
             if response and response[0:1] == b'\xF5':
-                ts, te, td, tv = calculate_sensor_status(response)
+                sensor_status.append(calculate_sensor_status(response))
 
     # Construct message
-    message = bytearray([0xAA, int(zone_id, 16), total_sensors, 0x01 if total_engaged > 0 else 0x00, 0x01 if total_disengaged > 0 else 0x00, total_engaged, total_disengaged, total_vacancy, 0x55])
+    total_sensors = len(sensor_status)
+    total_engaged = sensor_status.count(1)
+    total_disengaged = sensor_status.count(0)
+    total_vacancy = total_sensors - total_engaged - total_disengaged
+    message = bytearray([0xAA, int(zone_id, 16), total_sensors] + sensor_status + [total_engaged, total_disengaged, total_vacancy, 0x55])
     uart1.write(message)
 
 while True:
